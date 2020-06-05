08537a8768ddf6261a38223c7bf4f16fac960fbf
Jaco Greeff <jacogr@gmail.com>
2020-06-05 14:31:02 +0200

    Seperate deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git config --global push.default simple
+git config --global merge.ours.driver true
+git config --global user.name "Github Actions"
+git config --global user.email "action@github.com"
+
+yarn ghpages --repo https://${GH_PAT}@github.com/${GITHUB_REPOSITORY}.git --dist build
