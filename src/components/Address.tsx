97085cf091169bb7c796eccc3e4443fbbd640b34
Jaco Greeff <jacogr@gmail.com>
2020-06-05 11:56:32 +0200

    Known addresses for managers

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -9,6 +9,12 @@ interface Props {
 }
 
 const KNOWN: Record<string, string> = {
+  '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY': 'Alice',
+  '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty': 'Bob',
+  '5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y': 'Charlie',
+  '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy': 'Dave',
+  '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw': 'Eve',
+  '5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL': 'Ferdie',
   '5CPnByRewLBGFt4tCg5HruzJLTQEFj1xRDdFfPeaA2xVPeaa': 'Treasury'
 }
 
